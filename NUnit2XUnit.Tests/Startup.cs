c295142de2d2226b2345173a94de12d9a996c8c8
ku3mich <ku3mich@gmail.com>
2023-06-10 11:30:26 +0300

    fixed tests, coverage report

--- NUnit2XUnit.Tests/Startup.cs
+++ NUnit2XUnit.Tests/Startup.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Text.Diff;
-using XUnit.Extensions.Essentials;
+using Xunit.Extensions.Essentials;
 
 namespace NUnit2XUnit.Tests
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services) => services
-            .AddXUnitEssentials()
+        public void ConfigureServices(IServiceCollection services, HostBuilderContext hostContext) => services
+            .AddXunitEssentials(hostContext)
             .AddSingleton<ITextDiff, TextDiff>();
     }
 }
